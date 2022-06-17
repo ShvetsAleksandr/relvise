c10c699f09f7cc4a8b88a168636344eeb8ab7f10
SL1ppeR <slippersasha@gmail.com>
2022-06-17 14:26:14 +0300

    final version

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
+"use strict"
 const icons = document.querySelectorAll('.icon');
 const headerMenu = document.querySelector('.header__menu');
 const body = document.querySelector('body');
+let spollerTitles = document.querySelectorAll('.spoller__title');
+let spollerBlock = document.querySelectorAll('.spoller__block');
 
-icons.forEach (icon => {  
+icons.forEach(icon => {
    icon.addEventListener('click', (event) => {
-   icon.classList.toggle("open");
-   headerMenu.classList.toggle('active');
-   body.classList.toggle('hold');
+      icon.classList.toggle("open");
+      headerMenu.classList.toggle('active');
+      body.classList.toggle('hold');
    });
 });
+
+for (let title of spollerTitles) {
+   title.addEventListener('click', function (event) {
+      for (let item of spollerBlock) {
+         if (item == title.nextElementSibling) {
+         } else item.classList.remove('spoller__block-active');
+      }
+      for (let item of spollerTitles) {
+         if (item == this) {
+         } else item.classList.remove('spoller__title-active');
+      }
+      let block = title.nextElementSibling;
+      block.classList.toggle("spoller__block-active");
+      title.classList.toggle("spoller__title-active");
+   });
+};
+
